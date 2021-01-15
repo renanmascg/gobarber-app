8391f8cf44d166b3176e947d6ab4193ac9355ee0
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-01-15 15:36:16 -0300

    Adiciona validação de campos dentro do frontend da aplicação

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -17,7 +17,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const { fieldName, registerField } = useField(name);
+  const { fieldName, registerField, error } = useField(name);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +48,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         type="text"
         {...rest}
       />
+
+      {error}
     </Container>
   );
 };
