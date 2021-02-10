9f56ff52105b20474b1b9fbd124f1c5f2451a7fa
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-02-10 18:43:43 -0300

    Formata melhor a estrutura de pastas e adiciona o signout dentro do app

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -14,6 +14,7 @@ interface AuthState {
 interface IAuthContext {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<IAuthContext>({} as IAuthContext);
@@ -47,8 +48,15 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
